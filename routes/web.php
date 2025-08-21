add5ee61f1519c8d0b91c5d32b514dd493db5262
Okumura <maishu525@gmail.com>
2025-08-21 10:55:29 +0900

    web.phpをコミット

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyPageController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
